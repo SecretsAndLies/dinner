2bd1c802557c11c18d06bcd7fab3bf8bd80b021a
SecretsAndLies <36555262+SecretsAndLies@users.noreply.github.com>
2021-12-23 20:21:10 -0800

    Create dinner.py
    first commit

---
+++ dinner.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import random
+number =random.randint(1,10)
+if number == 1:
+    Dinner ="Indian food"
+elif number == 2:
+    Dinner ="Sushi"
+elif number == 3:
+    Dinner = "Burgers"
+elif number == 4:
+    Dinner ="from a Bakery"
+elif number == 5:
+    Dinner ="Mexican food"
+elif number == 6:
+    Dinner ="Chinese food"
+elif number == 7:
+    Dinner ="Thai food"
+elif number == 8:
+    Dinner ="Pizza"
+elif number == 9:
+    Dinner ="Breakfast food"
+elif number == 10:
+    Dinner ="Italian food"
+    
+print("Tonight, you should order " + Dinner +"! Remember to save some for me!")
